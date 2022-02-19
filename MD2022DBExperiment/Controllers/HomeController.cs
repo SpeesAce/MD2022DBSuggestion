383774c71e5b2bc9982ee904faff0cc21cb3f346
Michael Depré <michael.depre@student.vives.be>
2022-02-19 13:12:30 +0100

    init commit

---
+++ MD2022DBExperiment/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MD2022DBExperiment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MD2022DBExperiment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MD2022DBExperiment/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MD2022DBExperiment.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public Administrator Admins { get;set }
+
+        public GroupLeaders Groupleaders { get;set }
+
+        public
+    }
+}
